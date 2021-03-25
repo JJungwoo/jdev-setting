6ef01831e8ee184bf1c8837d9a5c4df8ed77288d
JJungwoo <jungwoo5759@gmail.com>
2021-03-25 14:35:10 +0900

    cleanup: install.sh 운영체제 버전 확인 제대로 안되는 부분 수정

--- install.sh
+++ install.sh
@@ -3,14 +3,17 @@
 OS=`uname -s`
 
 if [ ${OS} == "Linux" ]; then
-	if [ -n "$(uname -v | grep Ubuntu)" ]; then
+	if [ -n "$(uname -v | grep Ubuntu)" ] || [ -n "$(cat /etc/lsb-release | grep Ubuntu)" ] ; then
+        echo "Install ubuntu package"
 		sudo apt-get install -y vim ctags cscope
-	elif [ -n "$(uname -v | grep Centos)" ]; then
+	elif [ -n "$(uname -v | grep Centos)" ] || [ -n "$(cat /etc/redhat-release | grep CentOS)" ] ; then
+        echo "Install centos package"
 		sudo yum install -y vim ctags cscope
 	else
 		echo "This OS not supported to install.sh"
 	fi
 elif [ ${OS} == "Darwin" ]; then
+    echo "Install macos package"
 	sudo brew install vim ctags cscope
 else
 	echo "This OS not supported to install.sh"
@@ -18,19 +21,27 @@ fi
 
 # vundle install
 if [ -d "~/.vim/bundle/Vundle.vin" ]; then
+    echo "Alreadly install vundle"
+else
+    echo "Install vundle"
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+echo "cp vimrc"
 cp .vimrc ~/.vimrc
 
 # custome command add
 
-if [ -d "~/bin/" ]; then
+if [ -e "~/bin/" ]; then
+    echo "exist bin directory"
+else
+    echo "create bin directory"
 	mkdir ~/bin/
 fi
 
 cp mkcscope.sh ~/bin/
 
+echo "set custom command"
 if [ ! -e "~/.zshrc" ]; then
 	cat command_list >> ~/.zshrc
 	. ~/.zshrc
